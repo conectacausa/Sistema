bf1e38d7df0790de85fff25ef1dab477548753f1
conectacausa <contato@conectacausa.com.br>
2026-01-17 18:51:27 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,18 @@ Route::domain('{sub}.conecttarh.com.br')
     ->middleware(['web', 'tenant'])
     ->group(function () {
 
-        // Define automaticamente o parâmetro {sub} para geração de URLs (route('...'))
-        Route::defaults('sub', request()->route('sub'))->group(function () {
+        Route::get('/', function () {
+            if (!auth()->check()) {
+                return redirect()->route('login');
+            }
+            return redirect()->route('dashboard');
+        })->name('home');
 
-            Route::get('/', function () {
-                if (!auth()->check()) {
-                    return redirect()->route('login');
-                }
-                return redirect()->route('dashboard');
-            })->name('home');
+        Route::get('/login', [LoginController::class, 'show'])->name('login');
+        Route::post('/login', [LoginController::class, 'login'])->name('login.post');
+        Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-            Route::get('/login', [LoginController::class, 'show'])->name('login');
-            Route::post('/login', [LoginController::class, 'login'])->name('login.post');
-            Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-            Route::middleware(['auth', 'tenant.user'])->group(function () {
-                Route::get('/dashboard', fn () => view('dashboard.index'))->name('dashboard');
-            });
-
+        Route::middleware(['auth', 'tenant.user'])->group(function () {
+            Route::get('/dashboard', fn () => view('dashboard.index'))->name('dashboard');
         });
     });
