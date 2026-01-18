682539e71a1078bae47138c7ba5701d32971c605
conectacausa <contato@conectacausa.com.br>
2026-01-18 18:12:28 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,15 @@ Route::domain('{sub}.conecttarh.com.br')
     ->group(function () {
 
         /**
-         * LOCALIZAÇÃO (GLOBAL / CENTRAL)
-         * NÃO passa por tenant.
+         * LOCALIZAÇÃO (GLOBAL)
+         * Não usar tenant para não alterar search_path/schema.
          */
         Route::get('/paises', [LocalizacaoApiController::class, 'paises'])->name('api.paises.index');
         Route::get('/paises/{paisId}/estados', [LocalizacaoApiController::class, 'estadosByPais'])->name('api.estados.byPais');
         Route::get('/estados/{estadoId}/cidades', [LocalizacaoApiController::class, 'cidadesByEstado'])->name('api.cidades.byEstado');
 
         /**
-         * FILIAIS (DO TENANT)
-         * Passa por tenant.
+         * FILIAIS (TENANT)
          */
         Route::middleware(['tenant', 'tenant.user'])->group(function () {
             Route::get('/filiais', [FiliaisApiController::class, 'index'])->name('api.filiais.index');
