eb544004c60a0d4fca9cc822326ec9322404e40f
conectacausa <contato@conectacausa.com.br>
2026-01-18 17:13:15 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FiliaisApiController;
 use App\Http\Controllers\Api\LocalizacaoApiController;
 
-Route::middleware(['auth:sanctum', 'screen:5'])->group(function () {
+Route::domain('{sub}.conecttarh.com.br')
+    ->middleware(['web', 'tenant'])
+    ->group(function () {
 
-    // Listagem com filtros + paginação
-    Route::get('/filiais', [FiliaisApiController::class, 'index'])->name('api.filiais.index');
+        Route::prefix('api')
+            ->middleware(['auth', 'tenant.user', 'screen:5'])
+            ->group(function () {
 
-    // Soft delete
-    Route::delete('/filiais/{filial}', [FiliaisApiController::class, 'destroy'])->name('api.filiais.destroy');
+                // FILIAIS
+                Route::get('/filiais', [FiliaisApiController::class, 'index'])->name('api.filiais.index');
+                Route::delete('/filiais/{filial}', [FiliaisApiController::class, 'destroy'])->name('api.filiais.destroy');
 
-    // Combos dependentes
-    Route::get('/paises', [LocalizacaoApiController::class, 'paises'])->name('api.paises.index');
-    Route::get('/paises/{pais}/estados', [LocalizacaoApiController::class, 'estadosByPais'])->name('api.estados.byPais');
-    Route::get('/estados/{estado}/cidades', [LocalizacaoApiController::class, 'cidadesByEstado'])->name('api.cidades.byEstado');
-});
+                // LOCALIZAÇÃO
+                Route::get('/paises', [LocalizacaoApiController::class, 'paises'])->name('api.paises.index');
+                Route::get('/paises/{pais}/estados', [LocalizacaoApiController::class, 'estadosByPais'])->name('api.estados.byPais');
+                Route::get('/estados/{estado}/cidades', [LocalizacaoApiController::class, 'cidadesByEstado'])->name('api.cidades.byEstado');
+            });
+    });
