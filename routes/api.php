3616ed5ebcc10a37c2b89124a34c6db7f6cf4fd7
conectacausa <contato@conectacausa.com.br>
2026-01-18 18:10:22 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,23 @@ use App\Http\Controllers\Api\FiliaisApiController;
 use App\Http\Controllers\Api\LocalizacaoApiController;
 
 Route::domain('{sub}.conecttarh.com.br')
-    ->middleware(['web', 'tenant', 'auth', 'tenant.user', 'screen:5'])
+    ->middleware(['web', 'auth', 'screen:5'])
     ->group(function () {
 
-        // FILIAIS
-        Route::get('/filiais', [FiliaisApiController::class, 'index'])->name('api.filiais.index');
-        Route::delete('/filiais/{filial}', [FiliaisApiController::class, 'destroy'])->name('api.filiais.destroy');
-
-        // LOCALIZAÇÃO (por ID, sem model binding)
+        /**
+         * LOCALIZAÇÃO (GLOBAL / CENTRAL)
+         * NÃO passa por tenant.
+         */
         Route::get('/paises', [LocalizacaoApiController::class, 'paises'])->name('api.paises.index');
         Route::get('/paises/{paisId}/estados', [LocalizacaoApiController::class, 'estadosByPais'])->name('api.estados.byPais');
         Route::get('/estados/{estadoId}/cidades', [LocalizacaoApiController::class, 'cidadesByEstado'])->name('api.cidades.byEstado');
+
+        /**
+         * FILIAIS (DO TENANT)
+         * Passa por tenant.
+         */
+        Route::middleware(['tenant', 'tenant.user'])->group(function () {
+            Route::get('/filiais', [FiliaisApiController::class, 'index'])->name('api.filiais.index');
+            Route::delete('/filiais/{filial}', [FiliaisApiController::class, 'destroy'])->name('api.filiais.destroy');
+        });
     });
