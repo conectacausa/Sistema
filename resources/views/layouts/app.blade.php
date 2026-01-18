1f1bc8c74f8f5fe65bc7bf9b00340246032d63b6
conectacausa <contato@conectacausa.com.br>
2026-01-17 21:01:52 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,64 +1,68 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}">
-  <title>@yield('title', 'ConecttaRH')</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="{{ asset('assets/css/vendors_css.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/skin_color.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+    <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+    <title>
+        {{ ($tenant->nome_fantasia ?? 'Conectta RH') }} - @yield('title', 'Dashboard')
+    </title>
+
+    <!-- Vendors Style -->
+    <link rel="stylesheet" href="{{ asset('assets/css/vendors_css.css') }}">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/skin_color.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 </head>
 
 <body class="hold-transition light-skin sidebar-mini theme-primary fixed">
+
 <div class="wrapper">
-  <div id="loader"></div>
+    <div id="loader"></div>
 
-  @include('partials.header')
-  @include('partials.menu')
+    {{-- HEADER --}}
+    @include('partials.header')
 
-  <div class="content-wrapper overflow-visible">
-    <div class="container-full">
-      <section class="content">
-        @yield('content')
-      </section>
+    {{-- MENU --}}
+    @include('partials.menu')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper overflow-visible">
+        <div class="container-full">
+            @yield('content')
+        </div>
     </div>
-  </div>
+    <!-- /.content-wrapper -->
 
-  @include('partials.footer')
+    {{-- FOOTER --}}
+    @include('partials.footer')
 
-  <div class="control-sidebar-bg"></div>
+    <div class="control-sidebar-bg"></div>
 </div>
 
+<!-- Vendor JS -->
 <script src="{{ asset('assets/js/vendors.min.js') }}"></script>
 <script src="{{ asset('assets/js/pages/chat-popup.js') }}"></script>
 <script src="{{ asset('assets/icons/feather-icons/feather.min.js') }}"></script>
 
-{{-- Toastr (já existe no seu template de login) --}}
-<script src="{{ asset('assets/js/pages/toastr.js') }}"></script>
-<script src="{{ asset('assets/js/pages/notification.js') }}"></script>
+<script src="{{ asset('assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
+<script src="{{ asset('assets/vendor_components/jvectormap/lib2/jquery-jvectormap-2.0.2.min.js') }}"></script>
+<script src="https://fastly.jsdelivr.net/npm/echarts@5.5.1/dist/echarts.min.js"></script>
+<script src="{{ asset('assets/vendor_components/OwlCarousel2/dist/owl.carousel.js') }}"></script>
 
-<script>
-  // Toastr padrão do sistema (todas as telas)
-  @if(session('toastr'))
-    (function () {
-      const t = @json(session('toastr'));
-      // t: {type:'success|error|info|warning', message:'...'}
-      if (window.toastr && t?.message) {
-        toastr.options.closeButton = true;
-        toastr.options.progressBar = true;
-        toastr.options.timeOut = 4000;
-        toastr[t.type || 'info'](t.message);
-      } else if (t?.message) {
-        alert(t.message);
-      }
-    })();
-  @endif
-</script>
+<!-- DataTables JS (igual ao template) -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
+<!-- App -->
+<script src="{{ asset('assets/js/demo.js') }}"></script>
+<script src="{{ asset('assets/js/template.js') }}"></script>
 
 @stack('scripts')
+
 </body>
 </html>
