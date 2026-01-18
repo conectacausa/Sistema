bc442960c031cefb2a2f3cb257a676c3484f1d1d
conectacausa <contato@conectacausa.com.br>
2026-01-18 17:46:13 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ Route::domain('{sub}.conecttarh.com.br')
         Route::get('/filiais', [FiliaisApiController::class, 'index'])->name('api.filiais.index');
         Route::delete('/filiais/{filial}', [FiliaisApiController::class, 'destroy'])->name('api.filiais.destroy');
 
-        // LOCALIZAÇÃO
+        // LOCALIZAÇÃO (usando IDs, sem model binding)
         Route::get('/paises', [LocalizacaoApiController::class, 'paises'])->name('api.paises.index');
-        Route::get('/paises/{pais}/estados', [LocalizacaoApiController::class, 'estadosByPais'])->name('api.estados.byPais');
-        Route::get('/estados/{estado}/cidades', [LocalizacaoApiController::class, 'cidadesByEstado'])->name('api.cidades.byEstado');
+        Route::get('/paises/{paisId}/estados', [LocalizacaoApiController::class, 'estadosByPais'])->name('api.estados.byPais');
+        Route::get('/estados/{estadoId}/cidades', [LocalizacaoApiController::class, 'cidadesByEstado'])->name('api.cidades.byEstado');
     });
