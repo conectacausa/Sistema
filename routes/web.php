7a55281b4b08bdb3d57b34c7b719ee6aeb23f3bf
conectacausa <contato@conectacausa.com.br>
2026-01-18 17:04:11 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Config\FiliaisController;
 
 Route::domain('{sub}.conecttarh.com.br')
     ->middleware(['web', 'tenant'])
@@ -19,6 +20,18 @@ Route::domain('{sub}.conecttarh.com.br')
         Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
         Route::middleware(['auth', 'tenant.user'])->group(function () {
+
             Route::get('/dashboard', fn () => view('dashboard.index'))->name('dashboard');
+
+            /**
+             * CONFIG / FILIAIS
+             * slug: config/filiais
+             * screen_id: 5 (autorização)
+             */
+            Route::middleware(['screen:5'])->group(function () {
+                Route::get('/config/filiais', [FiliaisController::class, 'index'])->name('config.filiais.index');
+                Route::get('/config/filiais/nova', [FiliaisController::class, 'create'])->name('config.filiais.create');
+                Route::get('/config/filiais/{filial}/editar', [FiliaisController::class, 'edit'])->name('config.filiais.edit');
+            });
         });
     });
