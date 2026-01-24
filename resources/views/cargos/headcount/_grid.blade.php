3da2d70f4944a1e58050f69ef37c54466aa43912
conectacausa <contato@conectacausa.com.br>
2026-01-24 18:47:18 -0300

    Update _grid.blade.php

--- resources/views/cargos/headcount/_grid.blade.php
+++ resources/views/cargos/headcount/_grid.blade.php
@@ -1,5 +1,4 @@
 @php
-  // Totais do que está na tela (somatório das linhas do grupo)
   $totalAtual = 0;
   $totalIdeal = 0;
 
@@ -16,27 +15,23 @@
 
   $saldoTotal = $totalIdeal - $totalAtual;
 
-  // Por enquanto ainda não implementamos "Vagas Abertas"
+  // Por enquanto sem regra -> 0
   $vagasAbertas = 0;
 
-  // Percentuais da barra (só pra não quebrar visualmente)
   $pctAtual = $totalIdeal > 0 ? (int) round(($totalAtual / $totalIdeal) * 100) : 0;
-  if ($pctAtual < 0) $pctAtual = 0;
-  if ($pctAtual > 100) $pctAtual = 100;
+  $pctAtual = max(0, min(100, $pctAtual));
 
-  $pctIdeal = 100; // referência
+  $pctIdeal = 100;
+
   $pctSaldo = $totalIdeal > 0 ? (int) round((abs($saldoTotal) / $totalIdeal) * 100) : 0;
-  if ($pctSaldo < 0) $pctSaldo = 0;
-  if ($pctSaldo > 100) $pctSaldo = 100;
+  $pctSaldo = max(0, min(100, $pctSaldo));
 
   $pctVagas = 0;
 
-  // Classes de cor (Saldo: se atual > ideal, sinalizar)
-  $saldoClass = $totalAtual > $totalIdeal ? 'text-danger' : 'text-warning';
-  $saldoBarClass = $totalAtual > $totalIdeal ? 'bg-danger' : 'bg-warning';
+  $saldoTextClass = $totalAtual > $totalIdeal ? 'text-danger' : 'text-warning';
+  $saldoBarClass  = $totalAtual > $totalIdeal ? 'bg-danger' : 'bg-warning';
 @endphp
 
-{{-- CARDS (abaixo do filtro e acima da tabela) --}}
 <div class="row">
   <div class="col-12">
     <div class="box">
@@ -46,9 +41,7 @@
           <div class="box-body be-1 border-light">
             <div class="flexbox mb-1">
               <span>
-                <span class="icon-User fs-40">
-                  <span class="path1"></span><span class="path2"></span>
-                </span><br>
+                <span class="icon-User fs-40"><span class="path1"></span><span class="path2"></span></span><br>
                 Quadro Atual
               </span>
               <span class="text-primary fs-40">{{ $totalAtual }}</span>
@@ -65,9 +58,7 @@
           <div class="box-body be-1 border-light">
             <div class="flexbox mb-1">
               <span>
-                <span class="icon-Selected-file fs-40">
-                  <span class="path1"></span><span class="path2"></span>
-                </span><br>
+                <span class="icon-Selected-file fs-40"><span class="path1"></span><span class="path2"></span></span><br>
                 Quadro Disponível
               </span>
               <span class="text-info fs-40">{{ $totalIdeal }}</span>
@@ -84,12 +75,10 @@
           <div class="box-body be-1 border-light">
             <div class="flexbox mb-1">
               <span>
-                <span class="icon-Info-circle fs-40">
-                  <span class="path1"></span><span class="path2"></span><span class="path3"></span>
-                </span><br>
+                <span class="icon-Info-circle fs-40"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span><br>
                 Saldo
               </span>
-              <span class="{{ $saldoClass }} fs-40">{{ $saldoTotal }}</span>
+              <span class="{{ $saldoTextClass }} fs-40">{{ $saldoTotal }}</span>
             </div>
             <div class="progress progress-xxs mt-10 mb-0">
               <div class="progress-bar {{ $saldoBarClass }}" role="progressbar"
@@ -103,9 +92,7 @@
           <div class="box-body">
             <div class="flexbox mb-1">
               <span>
-                <span class="icon-Group-folders fs-40">
-                  <span class="path1"></span><span class="path2"></span>
-                </span><br>
+                <span class="icon-Group-folders fs-40"><span class="path1"></span><span class="path2"></span></span><br>
                 Vagas Abertas
               </span>
               <span class="text-danger fs-40">{{ $vagasAbertas }}</span>
@@ -123,5 +110,4 @@
   </div>
 </div>
 
-{{-- TABELA --}}
 @include('cargos.headcount._table', ['groups' => $groups])
