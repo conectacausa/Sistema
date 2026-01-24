971054309d5fc2227e9fbdc32bdc7c7ec41f7f9a
conectacausa <contato@conectacausa.com.br>
2026-01-24 10:39:56 -0300

    Create Cbo.php

---
+++ app/Models/Cbo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Cbo extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'cbos';
+
+    protected $fillable = [
+        'cbo',
+        'titulo',
+        'descricao',
+    ];
+
+    public function cargos(): HasMany
+    {
+        return $this->hasMany(Cargo::class, 'cbo_id');
+    }
+}
