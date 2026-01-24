6bc79bfbb3469a6c91a326647126c6c589b8007c
conectacausa <contato@conectacausa.com.br>
2026-01-24 10:40:17 -0300

    Create Cargo.php

---
+++ app/Models/Cargo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Cargo extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'cargos';
+
+    protected $fillable = [
+        'titulo',
+        'cbo_id',
+        'status',
+        'jovem_aprendiz',
+        'descricao_cargo',
+        'revisao',
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
+        'jovem_aprendiz' => 'boolean',
+        'revisao' => 'date',
+    ];
+
+    public function cbo(): BelongsTo
+    {
+        return $this->belongsTo(Cbo::class, 'cbo_id');
+    }
+}
