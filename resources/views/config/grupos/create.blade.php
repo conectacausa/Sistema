cd8b154c50ee8171d1e68cce82719aff6f3bdcbe
conectacausa <contato@conectacausa.com.br>
2026-01-31 20:28:35 -0300

    Update create.blade.php

--- resources/views/config/grupos/create.blade.php
+++ resources/views/config/grupos/create.blade.php
@@ -4,19 +4,15 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
   <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}">
 
   <title>{{ config('app.name', 'ConecttaRH') }} | Novo Grupo de Permissão</title>
 
-  <!-- Vendors Style-->
   <link rel="stylesheet" href="{{ asset('assets/css/vendors_css.css') }}">
-
-  <!-- Style-->
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/skin_color.css') }}">
 </head>
+
 <body class="hold-transition light-skin sidebar-mini theme-primary fixed">
 
 <div class="wrapper">
@@ -39,10 +35,8 @@
                     <a href="{{ route('dashboard') }}"><i class="mdi mdi-home-outline"></i></a>
                   </li>
                   <li class="breadcrumb-item">Configuração</li>
-                  <li class="breadcrumb-item">
-                    <a href="{{ route('config.grupos.index') }}">Grupos de Permissão</a>
-                  </li>
-                  <li class="breadcrumb-item active" aria-current="page">Novo Grupo</li>
+                  <li class="breadcrumb-item">Grupos de Permissão</li>
+                  <li class="breadcrumb-item active">Novo Grupo</li>
                 </ol>
               </nav>
             </div>
@@ -62,27 +56,24 @@
                 <form method="POST" action="{{ route('config.grupos.store') }}">
                   @csrf
 
-                  <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label class="form-label">Nome do Grupo</label>
-                        <input type="text"
-                               name="nome_grupo"
-                               class="form-control @error('nome_grupo') is-invalid @enderror"
-                               placeholder="Nome do Grupo"
-                               value="{{ old('nome_grupo') }}"
-                               maxlength="160"
-                               required>
-                        @error('nome_grupo')
-                          <div class="text-danger mt-1">{{ $message }}</div>
-                        @enderror
-                      </div>
-                    </div>
+                  <div class="form-group">
+                    <label class="form-label">Nome do Grupo</label>
+                    <input type="text"
+                           name="nome_grupo"
+                           class="form-control @error('nome_grupo') is-invalid @enderror"
+                           placeholder="Nome do Grupo"
+                           value="{{ old('nome_grupo') }}"
+                           maxlength="160"
+                           required>
+                    @error('nome_grupo')
+                      <div class="text-danger mt-1">{{ $message }}</div>
+                    @enderror
                   </div>
 
-                  {{-- ✅ Somente Salvar à direita --}}
-                  <div class="d-flex justify-content-end">
-                    <button type="submit" class="btn btn-primary waves-effect waves-light">
+                  {{-- Salvar verde à direita --}}
+                  <div class="d-flex justify-content-end mt-3">
+                    <button type="submit"
+                            class="waves-effect waves-light btn bg-gradient-success">
                       Salvar
                     </button>
                   </div>
@@ -101,9 +92,6 @@
 </div>
 
 <script src="{{ asset('assets/js/vendors.min.js') }}"></script>
-<script src="{{ asset('assets/js/pages/chat-popup.js') }}"></script>
-<script src="{{ asset('assets/icons/feather-icons/feather.min.js') }}"></script>
-<script src="{{ asset('assets/js/demo.js') }}"></script>
 <script src="{{ asset('assets/js/template.js') }}"></script>
 </body>
 </html>
