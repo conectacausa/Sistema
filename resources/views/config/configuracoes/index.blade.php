f1c778057919748f54c4b6577567527502a1e679
conectacausa <contato@conectacausa.com.br>
2026-02-07 19:14:39 -0300

    Create index.blade.php

---
+++ resources/views/config/configuracoes/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="content-wrapper">
+  <div class="container-full">
+    <section class="content">
+      <div class="row">
+        <div class="col-12">
+          <div class="box">
+            <div class="box-body">
+              <h4>Configurações</h4>
+              <p class="text-muted">Tela central /config</p>
+
+              <div class="alert alert-info">
+                View carregada com sucesso: <b>resources/views/config/configuracoes/index.blade.php</b>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+@endsection
