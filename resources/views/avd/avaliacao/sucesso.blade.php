5db8c1f9506c7ab522e0ea9795ffd34b856d4f54
conectacausa <contato@conectacausa.com.br>
2026-02-14 10:47:28 -0300

    Create sucesso.blade.php

---
+++ resources/views/avd/avaliacao/sucesso.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.public')
+
+@section('title', 'Avaliação enviada')
+
+@section('content')
+<section class="content">
+  <div class="row justify-content-center">
+    <div class="col-12 col-lg-8">
+      <div class="box">
+        <div class="box-body">
+          <h3 class="m-0 mb-2">Obrigado!</h3>
+          <div class="text-muted">Sua avaliação foi enviada com sucesso.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+@endsection
